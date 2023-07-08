fd6b92ec4e2e1cc09dbee2e3cbfe5c624606d7c9
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2023-07-07 20:37:24 -0600

    Creando Rutas para cada Controlador

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,21 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AttendanceController;
+use App\Http\Controllers\Api\V1\CantonController;
+use App\Http\Controllers\Api\V1\CareerController;
+use App\Http\Controllers\Api\V1\CategoryController;
+use App\Http\Controllers\Api\V1\DepartmentController;
+use App\Http\Controllers\Api\V1\HamletController;
+use App\Http\Controllers\Api\V1\MunicipalityController;
+use App\Http\Controllers\Api\V1\NationalityController;
+use App\Http\Controllers\Api\V1\PositionController;
+use App\Http\Controllers\Api\V1\RegistrationController;
+use App\Http\Controllers\Api\V1\SchoolCenterController;
+use App\Http\Controllers\Api\V1\SectionController;
+use App\Http\Controllers\Api\V1\SpecialtyController;
+use App\Http\Controllers\Api\V1\StudentController;
+use App\Http\Controllers\Api\V1\TeacherController;
+use App\Http\Controllers\Api\V1\ZoneController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +30,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::apiResource('v1/asistencias', AttendanceController::class);
+Route::apiResource('v1/cantones', CantonController::class);
+Route::apiResource('v1/carreras', CareerController::class);
+Route::apiResource('v1/categorias', CategoryController::class);
+Route::apiResource('v1/departamentos', DepartmentController::class);
+Route::apiResource('v1/caserios', HamletController::class);
+Route::apiResource('v1/municipios', MunicipalityController::class);
+Route::apiResource('v1/nacionalides', NationalityController::class);
+Route::apiResource('v1/cargos', PositionController::class);
+Route::apiResource('v1/matriculas', RegistrationController::class);
+Route::apiResource('v1/centrosescolares', SchoolCenterController::class);
+Route::apiResource('v1/secciones', SectionController::class);
+Route::apiResource('v1/especialidades', SpecialtyController::class);
+Route::apiResource('v1/alumnos', StudentController::class);
+Route::apiResource('v1/profesores', TeacherController::class);
+Route::apiResource('v1/zonas', ZoneController::class);
