3d22e1fd4249200ee088503d8eed0b32d0aae50a
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2023-07-26 22:04:26 -0600

    Creando Controlador de Registro de Estados(RegistrationStatuController) y añadiendo esta ruta al archivo de rutas

---
+++ app/Http/Controllers/Api/V1/RegistrationStatuController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\RegistrationStatu;
+use Illuminate\Http\Request;
+
+class RegistrationStatuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\RegistrationStatu  $registrationStatu
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RegistrationStatu $registrationStatu)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\RegistrationStatu  $registrationStatu
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RegistrationStatu $registrationStatu)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\RegistrationStatu  $registrationStatu
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RegistrationStatu $registrationStatu)
+    {
+        //
+    }
+}
