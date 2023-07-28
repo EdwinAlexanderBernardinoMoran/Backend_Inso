b5c5b1dcbde6ac0041bdff6f426c7e5a72c92695
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2023-07-27 20:52:25 -0600

    Creando Resources y Collection para el controlador RegistrationStatuController, Agregando la funcion para modificar a la hora de mapear la fecha en el modelo RegistrationStatu

---
+++ app/Http/Resources/V1/Collection/RegistrationStatuCollection.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\V1\Collection;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class RegistrationStatuCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'data' => $this->collection,
+            'meta' => [
+                'organization' => 'Development(Edwin Alexander Bernardino Morán)',
+                'authors' => [
+                    'Edwin Alexander Bernardino Morán',
+                    'Development(Edwin Alexander Bernardino Morán)'
+                ]
+            ],
+            'type' => 'Estados De Matriculas (Instituto Nacional De Sonzacate)'
+        ];
+    }
+}
---
+++ app/Http/Resources/V1/Resources/RegistrationStatuResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\V1\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RegistrationStatuResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'status' => $this->status,
+            'created_at' => $this->published_at
+        ];
+    }
+}
